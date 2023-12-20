5937be7f1fd00927ca8171b4c3f8d99fe93f9ad3
abhishek <shindeabbhi73131@gmail.com>
2023-12-20 12:41:55 +0530

    first-commit

--- functions.php
+++ functions.php
@@ -28,8 +28,11 @@ function hello_elementor_child_enqueue_scripts() {
 	wp_enqueue_script( 'custom.js', get_stylesheet_directory_uri() . '/customjs/custom.js', array('jquery'), null, false );
 	//wp_enqueue_script('customjs', get_stylesheet_directory_uri() . '/customjs/custom.js', array(), (string) time(), 'true' );
 	wp_localize_script( 'custom.js', 'ajax_object', array(
-        'ajax_urls' => admin_url( 'admin-ajax.php' ),
+        'ajax_url' => admin_url( 'admin-ajax.php' ),
         'nonce'    => wp_create_nonce( 'ajax_nonce' ),
     ) );
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+
+
+"for testing";
